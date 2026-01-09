084cd42150f839a3318c8fef03508b9aee1519d5
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-01-09 11:10:09 +0200

    sumOfPositives

---
+++ sum-of-positives.js
@@ -0,0 +1,12 @@
+//You get an array of numbers, return the sum of all of the positives ones.
+
+function getSumOfPositiveNums(arr){
+    let sum = 0;
+     for (let i = 0; i < arr.length; i++){
+         if(arr[i] > 0)
+          sum+=arr[i];
+     }
+  return sum;
+}
+const array = [1,-4,7,12];
+console.log(getSumOfPositiveNums(array));
