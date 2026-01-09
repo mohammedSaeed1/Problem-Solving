ed9584e91584d4f7b3e1794578db0bb16d0d0266
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-01-09 17:31:25 +0200

    string-repeat

---
+++ string-repeat.js
@@ -0,0 +1,13 @@
+// Write a function that accepts a non-negative integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+function stringRepeat(n , s){
+     if(n >= 1){
+         let cartoona = ``;
+       for (let i = 0; i < n; i++)
+           cartoona+=s;
+       return cartoona;
+     }
+     return '';
+}
+
+
+stringRepeat(7,"Mohamed");
