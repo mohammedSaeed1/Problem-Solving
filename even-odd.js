dc05582e2b146c19b1b77816d79c6608ee7e1b3a
mohammedSaeed1 <mohamedseso2002@gmail.com>
2026-01-09 10:48:58 +0200

    first commit

---
+++ even-odd.js
@@ -0,0 +1,6 @@
+// 1- Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function evenOdd(number){
+    return number%2==0 ? "Even" : "Odd";
+}
+
+console.log(evenOdd(10)); // "Even"
